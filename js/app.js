9d7e8da72105c276ea7acbf78348138b79308a2d
JERRY.N <99591930+jerryn13@users.noreply.github.com>
2022-08-05 22:37:21 +0800

    minsized

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-function app() {
-    if (localStorage.getItem("refresh_token")==null || localStorage.getItem("access_token")==null){
-        window.location.href="/login"
-        return;
-    };
-
+function ready(){
     let amis = amisRequire('amis/embed');
     const match = amisRequire('path-to-regexp').match;
 
@@ -159,4 +154,30 @@ function app() {
         location: state.location || state
       });
     });
-  }
+
+};
+function app() {
+    if (localStorage.getItem("refresh_token")==null || localStorage.getItem("access_token")==null){
+        window.location.href="/login"
+        return;
+    };
+    usecss("amis@1.9.1-beta.26/sdk/sdk.css");
+    usecss("amis@1.9.1-beta.26/sdk/helper.css");
+    
+    usejs("amis@1.9.1-beta.26/sdk/sdk.js") ;
+    usejs("vue@2.7.8/dist/vue.js") ;
+    usejs("history@4.10.1/umd/history.js") ;
+
+    document.write('<link rel="stylesheet" href="/app/app.css">');
+    document.write('<div id="root" class="app-wrapper"></div>');
+
+    window.onload=ready
+  };
+function usejs(name){
+    var jssrc="https://s1.ledotec.cn/webfront/cdn/npm/";
+    document.write('<script src="'+jssrc+name+'"></script>');
+};
+function usecss(name){
+    var csssrc="https://s1.ledotec.cn/webfront/cdn/npm/";
+    document.write('<link rel="stylesheet" href="'+csssrc+name+'">');
+}
